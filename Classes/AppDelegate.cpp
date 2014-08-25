c7eacefd38e43c869e214120a603e4a84bbb438d
Min Li <lnimi@hotmail.com>
2014-08-25 15:42:16 -0400

    help buttons

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -41,7 +41,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     CCEGLView::sharedOpenGLView()->setDesignResolutionSize(designSize.width, designSize.height, kResolutionFixedHeight);
 
     // turn on display FPS
-    pDirector->setDisplayStats(true);
+    //pDirector->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 70);
